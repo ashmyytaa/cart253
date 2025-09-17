35ed9041f25b98fea733ad8849305ae9e1ff200b
ashmyytaa <ashmi@live.ca>
2025-09-16 20:21:51 -0400

    played with variables and how to manipulate them

--- topics/variables/introducing-variables/js/script.js
+++ topics/variables/introducing-variables/js/script.js
@@ -12,6 +12,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    // Create the canvas
+    createCanvas(640, 640);
 
 }
 
@@ -20,5 +22,39 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
+
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+
+    //NORMAL FILL
+    // fill(255, 255, 0);
+
+    //NORMAL
+    //  ellipse(320, 320, 100, 100);
+
+    //NORMAL - using division of canvas to find the middle
+    //ellipse(width / 2, height / 2, 100, 100);
+
+    //MOVING CIRCLE
+    // ellipse(mouseX, mouseY, 100, 100);
+
+
+    //SIZE-CHANGING CIRCLE
+    //ellipse(width / 2, height / 2, mouseX, mouseY);
+
+
+    //COLOR-CHANGING CIRCLE
+    fill(mouseX, mouseY, 0);
+    // ellipse(width / 2, height / 2, 100, 100);
+    ellipse(mouseX, mouseY, 100, 100);
+
+
+
+
+    //  width always contains the width in pixels of the canvas(i.e.what you set it to in createCanvas())
+    // height always contains the height in pixels of the canvas(i.e.what you set it to in createCanvas())
+    pop();
 
 }
