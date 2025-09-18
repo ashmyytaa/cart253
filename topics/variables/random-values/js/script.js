ceb27c41606ccf4576eac46fa51a51c4cd4175c3
ashmyytaa <ashmi@live.ca>
2025-09-17 22:44:37 -0400

    did random temlpate

--- topics/variables/random-values/js/script.js
+++ topics/variables/random-values/js/script.js
@@ -8,10 +8,27 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+let randomFill = random(0, 255);
+//fill(randomFill);
+
+let r = random(0, 255);
+let g = random(0, 255);
+let b = random(0, 255);
+//fill(r, g, b);
+
+let x = random(0, width); // x is anywhere horizontally
+let y = random(0, height); // y is anywhere vertically
+ellipse(x, y, 100);
+
+let bug = {
+    x: 0,
+    y: 200,
+    size: 10
+};
+
+
 function setup() {
+    createCanvas(400, 400);
 
 }
 
@@ -20,5 +37,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
+    fill(randomFill);
+
+    // Add a random number between 1 and 5 to the big's position
+    // to make it move jerkily
+    bug.x += random(1, 5);
+    ellipse(bug.x, bug.y, bug.size);
 
 }
