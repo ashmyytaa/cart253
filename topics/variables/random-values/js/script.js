4f8adb74ca9a551ffcc3ac2b5d9215e18dc0bf0f
ashmyytaa <ashmi@live.ca>
2025-09-18 11:03:04 -0400

    played with random() features
    did random colors, movements

--- topics/variables/random-values/js/script.js
+++ topics/variables/random-values/js/script.js
@@ -8,18 +8,6 @@
 
 "use strict";
 
-let randomFill = random(0, 255);
-//fill(randomFill);
-
-let r = random(0, 255);
-let g = random(0, 255);
-let b = random(0, 255);
-//fill(r, g, b);
-
-let x = random(0, width); // x is anywhere horizontally
-let y = random(0, height); // y is anywhere vertically
-ellipse(x, y, 100);
-
 let bug = {
     x: 0,
     y: 200,
@@ -38,11 +26,24 @@ function setup() {
 */
 function draw() {
     background(0);
-    fill(randomFill);
+
+
+    // let randomFill = random(0, 255);
+    // fill(randomFill);
+
+    let r = random(0, 255);
+    let g = random(0, 255);
+    let b = random(0, 255);
+    fill(r, g, b);
+
+    let x = random(0, width); // x is anywhere horizontally
+    let y = random(0, height); // y is anywhere vertically
+    ellipse(x, y, 100);
+
 
     // Add a random number between 1 and 5 to the big's position
     // to make it move jerkily
     bug.x += random(1, 5);
-    ellipse(bug.x, bug.y, bug.size);
+    // ellipse(bug.x, bug.y, bug.size);
 
 }
