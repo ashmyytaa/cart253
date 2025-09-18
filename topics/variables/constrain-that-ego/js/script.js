dffa17b33eb8293a40331d2a316861b41eec0725
ashmyytaa <ashmi@live.ca>
2025-09-17 20:19:07 -0400

    started constrain ego template + constrained object value

---
+++ topics/variables/constrain-that-ego/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Constrain that Ego!
+ * Pippin Barr
+ * 
+ * This ego is out of control...
+ */
+
+"use strict";
+
+// My ego
+let ego = {
+    x: 200,
+    y: 200,
+    size: 1,
+    minSize: 0, //need this for constrain
+    maxSize: 400, //need this for constrain
+    fill: 200
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Updates (expands and darkens) and displays the ego
+ */
+function draw() {
+    // Pink background
+    background(255, 200, 200);
+
+    // Update the ego
+    // Fill gets darker, symbolizing the negative energy
+    ego.fill = ego.fill - 0.5;
+    // Size gets bigger, symbolizing taking up emotional space
+    ego.size = ego.size + 1;
+    // Constrain the ego to be within its defined minimum and maximum
+    ego.size = constrain(ego.size, ego.minSize, ego.maxSize);
+
+    // Draw the ego
+    push();
+    noStroke();
+    fill(ego.fill);
+    ellipse(ego.x, ego.y, ego.size);
+    pop();
+}
