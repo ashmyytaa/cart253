650908624c1191fbaed4445ea99fc5ae62a17bc2
ashmyytaa <ashmi@live.ca>
2025-09-23 15:40:20 -0400

    Did the first steps in creating my portrait.
    - Did the head, neck, eyes, nose and started the mouth. Did the canvas and added some colors. Just did the main shapes just to get started and will add details further on.

--- jams/art-jam/js/script.js
+++ jams/art-jam/js/script.js
@@ -2,8 +2,7 @@
  * ART JAM
  * Ashmitha Kanagiah
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Self Portrait
  */
 
 "use strict";
@@ -12,6 +11,10 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(1000, 600);
+
+
+
 
 }
 
@@ -20,5 +23,68 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(170, 66, 245);
+
+    //head
+    push();
+    fill(194, 142, 58);
+    noStroke();
+    ellipse(500, 250, 450, 450);
+    pop();
+
+    //neck
+    push();
+    fill(194, 142, 58);
+    noStroke();
+    rect(460, 300, 100, 600);
+
+
+    //left eyes
+    push();
+    fill('white');
+    noStroke();
+    ellipse(370, 200, 50, 30);
+    pop();
+
+    //right eyes
+    push();
+    fill('white');
+    noStroke();
+    ellipse(650, 200, 50, 30);
+    pop();
+
+    //left eye pupil
+    push();
+    fill('black');
+    noStroke();
+    ellipse(650, 200, 20, 20);
+    pop();
+
+
+    //right eye pupil
+    push();
+    fill('black');
+    noStroke();
+    ellipse(370, 200, 20, 20);
+    pop();
+
+
+    //nose
+    push();
+    fill(194, 142, 58);
+    stroke(138, 96, 28);
+    strokeWeight(3);
+    rect(500, 250, 30, 60);
+    pop();
+
+    //mouth
+    push();
+    fill(194, 142, 58);
+    stroke(138, 96, 28);
+    strokeWeight(3);
+    arc(500, 300, 150, 200, 0, PI + QUARTER_PI, OPEN);
+    pop();
+
+
 
 }
