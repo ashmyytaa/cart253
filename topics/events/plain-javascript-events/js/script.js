9cd00f2fb3a8e88ba09f43e247e0cfa659836a4d
ashmyytaa <ashmi@live.ca>
2025-09-28 23:10:11 -0400

    eventlistener

--- topics/events/plain-javascript-events/js/script.js
+++ topics/events/plain-javascript-events/js/script.js
@@ -7,11 +7,28 @@
  */
 
 "use strict";
+// The background colour
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#ffffff"
+    },
+    switchKey: 32 // Space bar
+
+}
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(400, 400);
+
+    // Start listening for key down
+    // It often makes sense to set up event listeners in setup()
+    // But there are other possibilities too
+    // Call changeBG when a keydown event happens:
+    window.addEventListener("keydown", changeBG);
 
 }
 
@@ -20,5 +37,24 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(bg.fill);
 
+
+}
+
+
+
+/**
+ * Switch the background
+ */
+function changeBG(event) { // NOTE the event parameter
+    // Use event.keyCode to check if they pressed the switching key...
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }
