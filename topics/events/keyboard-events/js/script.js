9b68425273e89de401c019c26be1e7eb177e6bf4
ashmyytaa <ashmi@live.ca>
2025-09-28 22:35:05 -0400

    played with mousePressed and keyReleased in various ways

--- topics/events/keyboard-events/js/script.js
+++ topics/events/keyboard-events/js/script.js
@@ -21,6 +21,11 @@ const ball = {
         white: "#ffffff",
         red: "#ff0000",
         blue: "#0000ff"
+    },
+    // Keys that control the ball's colour
+    keys: {
+        redKey: 82, // R
+        blueKey: 66 // B
     }
 }
 
@@ -43,4 +48,81 @@ function draw() {
     fill(ball.fill);
     ellipse(ball.x, ball.y, ball.size);
     pop();
+}
+
+/**
+ * Handle keypress:
+ * - r = make the ball red
+ * - b = make the ball blue
+ */
+
+
+
+
+//function keyPressed(event) {
+//   if (event.key === "r") {
+//   ball.fill = ball.fills.red;
+//  }
+//   else if (event.key === "b") {
+//      ball.fill = ball.fills.blue;
+//  }
+//}
+
+/**
+ * Handle keyreleased:
+ * - r or b = Set the ball back to default
+ */
+//function keyReleased(event) {
+// if (event.key === "r" || event.key === "b") {
+//     ball.fill = ball.fills.white;
+// }
+//}//
+
+
+/**
+ * Handle keypress:
+ * - R = make the ball red
+ * - B = make the ball blue
+ */
+//function keyPressed(event) {
+// if (event.keyCode === 82) {
+//     ball.fill = ball.fills.red;
+// }
+//   else if (event.keyCode === 66) {
+//       ball.fill = ball.fills.blue;
+//  }
+//}
+
+/**
+ * Handle keyreleased:
+ * - R or B = Set the ball back to default
+ */
+//function keyReleased(event) {
+//  if (event.keyCode === 82 || event.keyCode === 66) {
+//     ball.fill = ball.fills.white;
+// }
+//}
+
+/**
+ * Handle keypress:
+ * - R = make the ball red
+ * - B = make the ball blue
+ */
+function keyPressed(event) {
+    if (event.keyCode === ball.keys.redKey) {
+        ball.fill = ball.fills.red;
+    }
+    else if (event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.blue;
+    }
+}
+
+/**
+ * Handle keyreleased:
+ * - R or B = Set the ball back to default
+ */
+function keyReleased(event) {
+    if (event.keyCode === ball.keys.redKey || event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.white;
+    }
 }
