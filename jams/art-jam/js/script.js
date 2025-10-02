d4d5efb845f6eaf605fff6800f68b1aca376eb4d
ashmyytaa <ashmi@live.ca>
2025-10-01 20:53:39 -0400

    Removed hair accessories, was too tacky and finalized my background
    -Removed hair accessories as i didnt like the look of it
    -Found a way to make my background have a slow pace when generating bubbles (used frameCount() )
    -Played with shape sizes, colors and strokes to make it prettier

--- jams/art-jam/js/script.js
+++ jams/art-jam/js/script.js
@@ -18,7 +18,7 @@ const head = {
 let cheeks = {
     x1: 600,
     y1: 320,
-    size: 90,
+    size: 110,
     x2: 400,
     y2: 320,
     fill: "#c216612d",
@@ -34,12 +34,12 @@ let hair = {
     size: 200,
 
     left: {
-        x: 230,
+        x: 250,
         y: 130,
     },
 
     right: {
-        x: 770,
+        x: 750,
         y: 130,
     },
 
@@ -55,22 +55,16 @@ let hair = {
 let bowDetail = {
     fill: "#368dc4ff",
     stroke: "#133246ff",
+    size: 40,
+    bowMinSize: 30,
+    bowMaxSize: 80,
 }
 
 let bubbles = {
-    x1: 0,
-    x2: 1000,
-    x3: 0,
-    y1: 100,
-    y2: 300,
-    y3: 500,
-    fill: "#84d2ff13",
+    x: undefined,
+    y: undefined,
+    fill: "#84d2ff09",
     size: 50,
-};
-
-let hairAccessory = {
-    fill: "#ffffffff",
-    size: 10,
 };
 
 
@@ -88,6 +82,9 @@ function setup() {
 */
 function draw() {
 
+
+
+
     //function calling:
 
     //function where the user hovers their mouse over the portrait's cheeks, they change color
@@ -96,6 +93,8 @@ function draw() {
     hairChange();
     //function where transparent circles mimicking bubbles fills up the canvas until it turns the canvas into another color
     bubblesBackground();
+    //function that increses and decreses the size of the bow buttons
+    hairBowChange();
 
 
     //head - shape of an ellipse
@@ -132,7 +131,7 @@ function draw() {
     push();
     noFill();
     stroke(128, 22, 60, 200);
-    strokeWeight(3);
+    strokeWeight(5);
     arc(500, 380, 100, 90, 0, PI + TWO_PI, OPEN); //using arc again to create a smile 
     pop();
 
@@ -141,14 +140,14 @@ function draw() {
     push();
     noFill();
     stroke(74, 33, 24);
-    strokeWeight(1);
+    strokeWeight(3);
     arc(580, 200, 50, 20, PI + TWO_PI, TWO_PI); //using arc to create my eyebrows
     pop();
     //eyebrows - left side
     push();
     noFill();
     stroke(74, 33, 24);
-    strokeWeight(1);
+    strokeWeight(3);
     arc(420, 200, 50, 20, PI + TWO_PI, TWO_PI); //using arc to create my eyebrows
     pop();
 
@@ -212,7 +211,7 @@ function draw() {
     fill(bowDetail.fill);
     stroke(bowDetail.stroke);
     strokeWeight(1.5);
-    ellipse(300, 185, 60);
+    ellipse(300, 185, bowDetail.size);
     pop();
 
 
@@ -237,104 +236,11 @@ function draw() {
     fill(bowDetail.fill);
     stroke(bowDetail.stroke);
     strokeWeight(1.5);
-    ellipse(700, 185, 60);
+    ellipse(700, 185, bowDetail.size);
     pop();
 
 
 
-    //creating the hair accesories - left side
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(140, 100, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(155, 98, hairAccessory.size);
-    pop();
-
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(170, 90, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(185, 80, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(199, 69, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(210, 55, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(220, 40, hairAccessory.size);
-    pop();
-
-    //hair accesories - right side
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(750, 40, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(753, 50, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(760, 60, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(770, 70, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(785, 75, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(800, 78, hairAccessory.size);
-    pop();
-
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(818, 79, hairAccessory.size);
-    pop();
-
-    push();
-    fill(hairAccessory.fill);
-    noStroke();
-    ellipse(835, 75, hairAccessory.size);
-    pop();
 
 
 
@@ -382,20 +288,24 @@ function hairChange() {
 //function where it creates the circles to mimick bubbles, for it to be projected onto the backgorund
 function bubblesBackground() {
 
+    if (frameCount % 60 === 0) {
+        bubbles.x = random(0, width);
+        bubbles.y = random(0, height);
+    }
+
     push();
-    fill(bubbles.fill);
-    strokeWeight(0.5);
-    let x1 = random(1000);
-    let y1 = random(600);
-    // point(x1, y1);
-    ellipse(x1, y1, bubbles.size);
+    noFill();
+    stroke(bubbles.fill);
+    strokeWeight(3);
+    // bubbles.x = random(0, 1000);
+    // bubbles.y = random(0, 600);
+    ellipse(bubbles.x, bubbles.y, bubbles.size);
     pop();
 
-
-
-
 }
-
+//where where the size of the bow button changes
 function hairBowChange() {
+    bowDetail.size += 0.1;
+    bowDetail.size = constrain(bowDetail.size, bowDetail.bowMinSize, bowDetail.bowMaxSize);
 
 }
