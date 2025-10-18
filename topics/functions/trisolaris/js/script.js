bb5a9e03ba173b6d320fb4677a895b59885acec8
ashmyytaa <ashmi@live.ca>
2025-10-18 17:20:32 -0400

    finished trisolaris project

--- topics/functions/trisolaris/js/script.js
+++ topics/functions/trisolaris/js/script.js
@@ -21,40 +21,19 @@ function draw() {
     // Sky blue
     background("#87ceeb");
 
-    drawSun1();
-    drawSun2();
-    drawSun3();
+    // Draw the three suns of Trisolaris
+    drawSun(500, 100, 80);
+    drawSun(350, 180, 200);
+    drawSun(120, 100, 160);
 }
 
 /**
- * Draws the first sun
+ * Draws a sun at specific position and size
  */
-function drawSun1() {
+function drawSun(x, y, size) {
     push();
     noStroke();
     fill("#f99736");
-    ellipse(500, 100, 80);
-    pop();
-}
-
-/**
- * Draws the second sun
- */
-function drawSun2() {
-    push();
-    noStroke();
-    fill("#f99736");
-    ellipse(350, 180, 200);
-    pop();
-}
-
-/**
- * Draws the third sun
- */
-function drawSun3() {
-    push();
-    noStroke();
-    fill("#f99736");
-    ellipse(120, 100, 160);
+    ellipse(x, y, size);
     pop();
 }
