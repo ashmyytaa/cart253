169945f81d9635a99a08b8cf170c4daa39c9e841
ashmyytaa <ashmi@live.ca>
2025-10-30 14:03:00 -0400

    finished step 3 + step 4
    did the for of look in draw as well as create the array

--- challenges/arrays-challenge/js/script.js
+++ challenges/arrays-challenge/js/script.js
@@ -5,8 +5,7 @@
  * A ball that bounces around on the canvas
  */
 
-let ball1 = undefined; // Will create it with createBall()
-
+let balls = [];
 /**
  * Create the canvas and the ball
  */
@@ -22,19 +21,6 @@ function setup() {
  */
 function createBall() {
     // Create a ball object with appropriate properties
-    // const newBall = {
-    // Position and dimensions
-    //   x: 200,
-    //   y: 200,
-    //  size: 20,
-    // Colour
-    //  fill: "#000000",
-    // Movement
-    //  velocity: {
-    //     x: random(-5, 5),
-    //      y: random(-5, 5)
-    //  }
-    //   };
 
     let newBall = {
         x: random(0, width),
@@ -57,45 +43,58 @@ function createBall() {
 function draw() {
     background("#87ceeb");
 
-    moveBall();
-    bounceBall();
-    drawBall();
+    for (let newBall of balls) {
+        moveBall(newBall);
+        bounceBall(newBall);
+        drawBall(newBall);
+    }
+
+
 }
 
 /**
  * Moves the ball according to its velocity
  */
-function moveBall() {
-    ball1.x += ball1.velocity.x;
-    ball1.y += ball1.velocity.y;
+function moveBall(newBall) {
+    newBall.x += newBall.velocity.x;
+    newBall.y += newBall.velocity.y;
 }
 
 /**
  * Bounces the ball off the walls
  */
-function bounceBall() {
+function bounceBall(newBall) {
     // Check if the ball has reached the left or right
-    const bounceX = (ball1.x > width || ball1.x < 0);
+    const bounceX = (newBall.x > width || newBall.x < 0);
     // Check if the ball has reached the top or bottom
-    const bounceY = (ball1.y > height || ball1.y < 0);
+    const bounceY = (newBall.y > height || newBall.y < 0);
 
     // Handle bouncing horizontally
     if (bounceX) {
-        ball1.velocity.x *= -1;
+        newBall.velocity.x *= -1;
     }
     // Handle bouncing vertically
     if (bounceY) {
-        ball1.velocity.y *= -1;
+        newBall.velocity.y *= -1;
     }
 }
 
 /**
  * Draw the ball on the canvas
  */
-function drawBall() {
+function drawBall(newBall) {
     push();
     noStroke();
     fill(newBall.fill);
     ellipse(newBall.x, newBall.y, newBall.size);
     pop();
+}
+
+function mousePressed() {
+
+    let newBall = createBall();
+    balls.push(newBall);
+
+
+
 }
