6603943fe30391e6430d5c4a77041e094b3ecbe3
ashmyytaa <ashmi@live.ca>
2025-11-16 23:40:59 -0500

    completed the background of my underwater()

--- jams/variation-jam/js/script.js
+++ jams/variation-jam/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(900, 600)
 }
 
 
@@ -20,5 +20,51 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0)
 
+    underwater();
+
+}
+
+
+
+function underwater() {
+    background("#0d2431ff");
+    wave(300, 0.002, "#071c29ff", 1);
+    wave(600, 0.002, "#0c1b25ff", 2);
+    wave(900, 0.002, "#030f17ff", 3);
+
+}
+
+function ground() {
+    background('red')
+
+
+}
+
+function space() {
+    background(0)
+
+
+}
+
+
+function wave(waveHeight, waveScale, waveColor, waveAddition) {
+    let noiseLevel = waveHeight; // to determine how tall the wave will go
+    let noiseScale = waveScale; // to determe how wavy it will be along the x-axis
+
+    push(); // creates the wave
+    fill(waveColor);
+    noStroke();
+    beginShape();
+    for (let x = 0; x < width; x += waveAddition) { // checks along the x-axis to pick values to be added for the wave
+        let scale = noiseScale * x;
+        let move = noiseScale * frameCount; // framecount makes the wave move
+        let y = noiseLevel * noise(scale, move);
+        vertex(x, y);
+    }
+    vertex(width, height); // vertex is used to connect all the point to make up the wave
+    vertex(0, height);
+    endShape(CLOSE); // to close the shape between the two endpoints
+    pop();
 }
